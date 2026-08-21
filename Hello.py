5ae99f0ca346fe0ad92263f897e48d7b32d4c5c3
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-08-21 22:17:43 +0530

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,5 +1,13 @@
 #ask user for their name
-name =  input("What's your name?")
+name =  input("What's your name?") # input function
+
+
+#remove the extra whitespace
+name = name.strip()
+
+#it captialize the all text
+name = name.capitalize()
 
 #Say hello to the user 
-print("Hello, " +name)
+#print("Hello",name)  #print funtion
+print(f"Hello, {name}")
