77a1a8290ace6f5bcbda478edecfc26361e1646a
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-16 23:45:24 +0530

    Problem set 1

---
+++ Octet_stream.py
@@ -0,0 +1,25 @@
+def main():
+    filename = input("File Name:").strip().lower()
+
+    if filename.endswith(".gif"):
+        print("image/gif")
+    elif filename.endswith(".jpg"):
+        print("image/jpg")
+    elif filename.endswith(".jpeg"):
+        print("image/jpeg")
+    elif filename.endswith(".png"):
+        print("image/png")
+    elif filename.endswith(".pdf"):
+        print("application/pdf")
+    elif filename.endswith(".txt"):
+        print("txt/plain")
+    elif filename.endswith(".zip"):
+        print("application/plain")
+    else:
+        print("application/octet-stream")
+
+if __name__ == "__main__":
+    main()
+
+
+
