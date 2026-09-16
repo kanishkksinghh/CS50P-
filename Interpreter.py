f4c229b6f6ccf07030a5f066fc6e0eff17ab3fd6
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-16 23:59:06 +0530

    Problem Set 1

---
+++ Interpreter.py
@@ -0,0 +1,14 @@
+def main():
+    expression = input("Expression:").strip()
+    
+x_str, y, z_str = expression.split("")
+
+x = float(x_str)
+z = float(z_str)
+
+if y == "+":
+    result = x+z:
+if y == "+":
+    result = x+z:
+        
+        
