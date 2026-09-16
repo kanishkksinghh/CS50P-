bed565111bd32ddef400c5be52ecfbbc2244dc4e
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-17 00:04:25 +0530

    Problem Set 1

--- Interpreter.py
+++ Interpreter.py
@@ -1,14 +1,22 @@
 def main():
-    expression = input("Expression:").strip()
+    expression = input("Expression: ").strip()
+
+    x_str, y, z_str = expression.split(" ")
+
+    x = float(x_str)
+    z = float(z_str)
     
-x_str, y, z_str = expression.split("")
-
-x = float(x_str)
-z = float(z_str)
-
-if y == "+":
-    result = x+z:
-if y == "+":
-    result = x+z:
+    if y == "+":
+        result = x + z
+    elif y == "-":
+        result = x - z
+    elif y == "*":
+        result = x * z
+    elif y == "/":
+        result = x / z
         
-        
+    print(f"{result:.1f}")
+
+
+if __name__ == "__main__":
+    main()
