70ef0d50cb28594bf36dd86cac1250da0a48a53d
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-18 11:18:10 +0530

    Regular Expressions

--- validate.py
+++ validate.py
@@ -2,7 +2,9 @@
 
 email = input("What's your email?").strip()
 
-if "@" in email:
+username, domain = email.split("@")
+
+if username and "." in domain:
     print("Valid")
 else:
-    print("Invlaid")
+    print("Invalid")
