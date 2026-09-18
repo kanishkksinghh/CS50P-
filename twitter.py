03511af5af33c21b17f2379f594e4ca3645b25aa
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-18 14:56:14 +0530

    Regular Expressions

--- twitter.py
+++ twitter.py
@@ -1,4 +1,7 @@
+import re
+
 url = input("URL: ").strip()
 
-username = url.replace("https://twwitter.com/", "")
-print(f"{username}")
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
+print(f"Username: {username}")
+
