022a9a5e8fb6587d351ea857665bc0358fa1de3c
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-19 15:06:25 +0530

    Object-Oriented Programming

--- Class.py
+++ Class.py
@@ -1,27 +1,24 @@
-class Student:  #Class
-    def __int__(self, name, house):
+class Student:
+    def __init__(self, name, house): 
         if not name:
             raise ValueError("Missing Value")
         if house not in ["Vikas", "GomtiNagar", "Lucknow"]:
             raise ValueError("Invalid House")
-            
         self.name = name
         self.house = house
-    
+
+    # This controls what prints when you print the object directly
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
-
+    print(student) 
 
 def get_student():
-    name = input("Name:")
-    house = input("house")
-    student = Student(name, house)
-    return student
-
-#methods classes comes with certain methods
-
-    
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
