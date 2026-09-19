cb69c5eb1a749a9114c40b804bc507994d094836
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-19 23:00:46 +0530

    Object-Oriented Programming

---
+++ wizard.py
@@ -0,0 +1,14 @@
+class Sutdent:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.house = house
+        
+class Professor:
+    def __init__(self, name, subject):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.subject = subject
+        
