2938dbe3b56f73276eb6dbbb80046f238983bfbc
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-19 23:04:11 +0530

    Object-Oriented Programming
    Parent class and Child class

--- wizard.py
+++ wizard.py
@@ -1,14 +1,19 @@
-class Sutdent:
+class Wizard: #parent class
+    def __init__(self, name):
+            if not name:
+                raise ValueError("Missing name")
+            self.name = name
+            
+    ...
+
+
+class Sutdent(Wizard):    #child class
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
-        self.name = name
+        super().__init__(name)
         self.house = house
         
 class Professor:
     def __init__(self, name, subject):
-        if not name:
-            raise ValueError("Missing name")
-        self.name = name
+        super().__init__(name)
         self.subject = subject
         
