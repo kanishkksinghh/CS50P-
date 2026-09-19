1752f36c2179fbfa6e2fc163d74e2472a8e8b09b
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-19 13:59:40 +0530

    Add files via upload

--- studentobp.py
+++ studentobp.py
@@ -1,12 +1,12 @@
 def main():
-    name = get_name()
-    house = get_house()
+    name, house = get_student()
     print(f"Hello, {name} from {house}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    return name, house
     
 
 if __name__ == "__main__":
