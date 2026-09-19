a7c8a2f28a980b466c03958d29693abced487e37
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-19 15:18:31 +0530

    Object-Oriented Programming

--- Class.py
+++ Class.py
@@ -11,10 +11,18 @@ class Student:
     # This controls what prints when you print the object directly
     def __str__(self):
         return f"{self.name} from {self.house} also {self.local_area}"
+    def charm(self):
+        match self.local_area:
+            case "Dayal":
+                return "hehe"
+            case "VV":
+                return "blup blup"
+            
 
 def main():
     student = get_student()
-    print(student) 
+    print("Excepto Area!")
+    print(student.charm()) 
 
 def get_student():
     name = input("Name: ")
