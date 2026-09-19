a453adb57f2e92936775afcdd1a0b848ccf4d7ad
kanishkksinghh <contact.kanishkksingh@gmail.com>
2026-09-19 23:12:22 +0530

    Object-Oriented Programming

--- wizard.py
+++ wizard.py
@@ -1,19 +1,20 @@
-class Wizard: #parent class
-    def __init__(self, name):
-            if not name:
-                raise ValueError("Missing name")
-            self.name = name
-            
-    ...
+class Wizard:  # parent class
+    def __init__(self, name): 
+        if not name: 
+            raise ValueError("Missing name") 
+        self.name = name 
 
+class Student(Wizard):  # child class
+    def __init__(self, name, house): 
+        super().__init__(name) 
+        self.house = house 
 
-class Sutdent(Wizard):    #child class
-    def __init__(self, name, house):
-        super().__init__(name)
-        self.house = house
-        
-class Professor:
-    def __init__(self, name, subject):
-        super().__init__(name)
-        self.subject = subject
-        
+class Professor(Wizard): 
+    def __init__(self, name, subject): 
+        super().__init__(name) 
+        self.subject = subject 
+
+# Object Instantiation
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
